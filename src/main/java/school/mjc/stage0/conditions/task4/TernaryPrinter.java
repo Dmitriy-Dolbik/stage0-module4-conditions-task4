d432a39515e65d04f91603788a3ee1cd0348c5d4
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-26 16:48:49 +0300

    correct solution

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryPrinter {
     public static void main(String[] args) {
         TernaryPrinter ternaryPrinter = new TernaryPrinter();
-        ternaryPrinter.printWhichIsBigger(123,1232);
+        ternaryPrinter.printWhichIsBigger(123243,1232);
     }
     public void printWhichIsBigger(int first, int second) {
-        System.out.println(first > second ? first : second);
+        System.out.println(first > second ? "first" : "second");
     }
 }
